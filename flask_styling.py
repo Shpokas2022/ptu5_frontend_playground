15043dee7353f1d08f7fed96cc1ddd2cbd049d1d
Darius Vasionis <darius@vasionis.eu>
2022-10-27 10:30:58 +0300

    grid-as su stulpeliais

--- flask_styling.py
+++ flask_styling.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("styling/home.html")
 
-@app.route("/containers")
 def containers():
     return render_template("styling/containers.html")
+
+@app.route("/grid")
+
+def grid():
+    return render_template("styling/grid.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
